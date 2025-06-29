a5e83684ad4b23770ada2925f18f13bea75a4e59
zhangweifeng <zhang.weifeng@tslsmart.com>
2025-06-29 15:35:37 +0800

    feat: 添加路由

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nav style="display: flex; gap: 10px">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/movies">Movies</nuxt-link>
+      <nuxt-link to="https://vueschool.io">Vue School</nuxt-link>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Page: Home</div>
+</template>
+
+<style scoped></style>
