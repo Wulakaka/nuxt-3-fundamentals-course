9565a737f26128402cff99c51e4dd03bb4a612d2
zhangweifeng <zhang.weifeng@tslsmart.com>
2025-06-29 15:58:36 +0800

    feat: 增加动态路由

---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+const message = computed(() => {
+  return `I'm on team ${route.params.teamSlug} and my user id is ${route.params.userId}`;
+});
+</script>
+
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
