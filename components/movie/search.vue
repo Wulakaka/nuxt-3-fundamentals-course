f21ab2f89613492df38e4411a56b47b99cdf931f
zhang.weifeng <zhangweifeng@diinom.com>
2025-06-30 09:19:14 +0800

    feat: 移动 MovieSearch 到单独文件

---
+++ components/movie/search.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const query = ref("");
+const apiKey = "e46ce174";
+const movies = ref([]);
+async function search() {
+  const {Search} = await $fetch(
+    `http://www.omdbapi.com/?apikey=${apiKey}&s=${query.value}`
+  );
+  movies.value = Search;
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="search">
+      <input v-model="query" type="text" placeholder="Search for a movie..." />
+      <button type="submit">Search</button>
+    </form>
+    <ul style="list-style: none; display: flex; flex-wrap: wrap; gap: 10px">
+      <li v-for="movie in movies" :key="movie.imdbID">
+        <NuxtLink :to="{name: 'movies-id', params: {id: movie.imdbID}}">
+          <img :src="movie.Poster" :alt="movie.title" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
