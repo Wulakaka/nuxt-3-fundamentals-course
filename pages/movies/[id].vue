80ef63eaf714440003d9a4c26a25340c10826b72
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-02 18:31:02 +0800

    feat: 增加错误处理

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,13 +1,24 @@
 <script setup>
 const route = useRoute();
 
-const {data} = await useFetch(
+const {data, error} = await useFetch(
   `http://www.omdbapi.com/?apikey=e46ce174&i=${route.params.id}`,
   {
     key: `/movie/${route.params.id}`,
-    pick: ["Title", "Plot"],
+    pick: ["Title", "Plot", "Error"],
   }
 );
+
+if (error.value) {
+  console.error("Error fetching movie data:", error.value);
+}
+
+if (data.value.Error === "Incorrect IMDb ID.") {
+  showError({
+    statusCode: 404,
+    statusMessage: "Page not found",
+  });
+}
 </script>
 
 <template>
