87d6a81d2c869c3a08f5c93600324620a487cc62
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-04 11:13:52 +0800

    feat: 增加全局登录 state

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isLoggedIn = useIsLoggedIn();
+</script>
 
 <template>
   <div>
@@ -6,7 +8,8 @@
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/movies">Movies</nuxt-link>
       <nuxt-link to="https://vueschool.io">Vue School</nuxt-link>
-      hello
+      <nuxt-link v-if="!isLoggedIn" to="/login">Login</nuxt-link>
+      <a v-else href="#" @click.prevent="isLoggedIn = false">Logout</a>
     </nav>
     <slot />
   </div>
--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,34 @@
 definePageMeta({
   layout: "plain",
 });
+
+const isLoggedIn = useIsLoggedIn();
+
+function login() {
+  isLoggedIn.value = true;
+  useRouter().push("/");
+}
 </script>
 
 <template>
-  <div>Page: login</div>
+  <div>
+    <h1>Page: login</h1>
+    <form @submit.prevent="login">
+      <label>
+        <span>Username</span>
+        <input type="text" name="username" placeholder="Enter your username" />
+      </label>
+      <label>
+        <span>Password</span>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
