a1ec04a0b9eacc8ed1ee7827df71738f6908fdcb
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-07 10:54:00 +0800

    feat: 调整 swr 缓存时间

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     dirs: ["stores"],
   },
   routeRules: {
-    "/swr": {swr: true},
+    "/swr": {swr: 60},
     "/static": {static: true},
     "/spa": {ssr: false},
   },
