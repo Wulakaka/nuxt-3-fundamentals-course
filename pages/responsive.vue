bc11f82ed40d1d169d4b56f64ed005bc9ab7967b
张伟峰 <wulakaka@live.com>
2025-07-09 23:24:54 +0800

    feat: 更新响应式页面，添加图片修饰符并调整图片宽度

--- pages/responsive.vue
+++ pages/responsive.vue
@@ -1,8 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const left = 2213;
+const top = 402;
+const width = 800;
+const height = 800;
+const extract = `${left}_${top}_${width}_${height}`;
+
+const modifiers = ref({
+  // flip: true,
+  extract,
+  // blur: 5,
+  // rotate: 45,
+});
+</script>
 
 <template>
   <div>
-    <NuxtPicture format="webp" src="/cat.jpg" width="200" />
+    <NuxtPicture
+      format="webp"
+      src="/cat.jpg"
+      width="300"
+      fit="cover"
+      :modifiers="modifiers"
+    />
   </div>
 </template>
 
