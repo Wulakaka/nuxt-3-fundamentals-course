62200521836935df7eeea81c76247dfbc8b09048
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-09 09:16:56 +0800

    feat: 增加图片

---
+++ pages/responsive.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <NuxtPicture format="webp" src="/cat.jpg" width="200" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  display: block;
+  max-width: 100%;
+}
+</style>
