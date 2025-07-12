a458e8a60e1681633a1032e826bdb97170706f61
zhangweifeng <zhang.weifeng@tslsmart.com>
2025-07-12 17:02:32 +0800

    feat: 实现 useCycleList 组合函数，支持循环列表功能

---
+++ composables/useCycleList.ts
@@ -0,0 +1,79 @@
+export interface useCycleListConfig {
+  fallbackIndex?: number;
+  fallbackValue?: any;
+}
+
+const useCycleListConfigDefaults: useCycleListConfig = {
+  fallbackIndex: undefined,
+  fallbackValue: undefined,
+};
+
+export const useCycleList = <T>(
+  list: MaybeRefOrGetter<T[]>,
+  config?: useCycleListConfig
+) => {
+  const _config = {
+    ...useCycleListConfigDefaults,
+    ...config,
+  };
+
+  const _list = toRef(list) as Ref<T[]>;
+  const activeIndex = ref(0);
+
+  const state = computed({
+    get() {
+      return _list.value[activeIndex.value];
+    },
+    set(value) {
+      const foundIndex = _list.value.indexOf(value);
+      if (foundIndex !== -1) {
+        activeIndex.value = foundIndex;
+      } else {
+        if (typeof _config.fallbackValue !== "undefined") {
+          const foundFallbackIndex = _list.value.indexOf(_config.fallbackValue);
+          if (foundFallbackIndex !== -1) {
+            activeIndex.value = foundFallbackIndex;
+          } else {
+            throw new Error(
+              `Fallback value "${_config.fallbackValue}" not found in the list.`
+            );
+          }
+        } else {
+          throw new Error(`Value "${value}" not found in the list.`);
+        }
+      }
+    },
+  });
+
+  function add(index: number) {
+    activeIndex.value =
+      (activeIndex.value + index + _list.value.length) % _list.value.length;
+  }
+
+  function next() {
+    add(1);
+  }
+
+  function prev() {
+    add(-1);
+  }
+
+  function go(index: number) {
+    if (index >= _list.value.length) {
+      if (typeof _config.fallbackIndex !== "undefined") {
+        activeIndex.value = _config.fallbackIndex;
+      } else {
+        throw new Error(`Index ${index} is out of bounds for the list.`);
+      }
+    } else {
+      activeIndex.value = index;
+    }
+  }
+
+  return {
+    state,
+    prev,
+    next,
+    go,
+  };
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useCycleList} from "~/composables/useCycleList";
+
+const {prev, next, state, go} = useCycleList(
+  () => ["Dog", "Cat", "Fish", "Bird", "Hamster", "Rabbit"],
+  {
+    fallbackIndex: 0,
+    fallbackValue: "Cat",
+  }
+);
+
+state.value = "Hawk"; // Set initial state to "Cat"
+
+const goto = ref(0);
+</script>
 
 <template>
-  <div>Page: Home</div>
+  <div>
+    {{ state }}
+    <br />
+    <button @click="prev">Previous</button>
+    <button @click="next">Next</button>
+    <br />
+    <input v-model="goto" type="number" />
+    <button @click="go(goto)">go to {{ goto }}</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@reference "tailwindcss";
+
+button {
+  @apply text-white px-4 py-2 rounded border border-gray-300 bg-blue-500 hover:bg-blue-600;
+}
+</style>
